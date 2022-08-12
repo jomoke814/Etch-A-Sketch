5df654fde46ebdbee4ce446fd2d6280e458d42cf
jomoke814 <jcullen814@gmail.com>
2022-08-12 17:20:21 +0300

    Creating Grid and Adding Style to divs and also adding eventListener to grid

--- js/etch.js
+++ js/etch.js
@@ -1,1 +1,24 @@
-console.log('Welcome to etch a sketch')
+let sizeGrid = 16;
+const container = document.querySelector ('.container')
+const createGrid = (squareGrids) => {
+    for(let i = 0; i < squareGrids; i++){
+        const row = document.createElement('div')
+        row.classList.add('row-grid')
+        
+        for(let j = 0; j < squareGrids; j++ ) {
+            const widthAndHeight = 900 / sizeGrid
+            const boxGrid = document.createElement('div')
+            boxGrid.classList.add('box-grid')
+            boxGrid.style.width = `${widthAndHeight}px`
+            boxGrid.style.height = `${widthAndHeight}px`
+
+            boxGrid.addEventListener('mouseenter', () => {
+                boxGrid.style.backgroundColor = 'black'
+            })
+            row.appendChild(boxGrid)
+        }
+        container.appendChild(row)
+    }
+}
+
+createGrid(sizeGrid)
